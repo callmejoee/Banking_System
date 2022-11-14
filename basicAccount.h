a36f854703893c78395356a06ee7e5d86c583e48
Yousef M. Eldaly <94533646+YousefEldaly@users.noreply.github.com>
2022-11-14 17:13:42 +0200

    Add files via upload

---
+++ basicAccount.h
@@ -0,0 +1,30 @@
+#ifndef BASICACCOUNT_H_INCLUDED
+#define BASICACCOUNT_H_INCLUDED
+//#include"clientClass.h"
+#include <iostream>
+
+using namespace std;
+
+
+class bankAccount
+{
+private:
+    int accountID;
+    float balance;
+
+public:
+    bankAccount();
+    void set_accountID(int);
+    void set_balance(float balance);
+
+    int get_accountID();
+    float get_balance();
+
+    void deposit(float amount);
+    void withdraw(float amount);
+
+    void display();
+};
+
+
+#endif // BASICACCOUNT_H_INCLUDED
---
+++ clientClass.h
@@ -0,0 +1,49 @@
+#ifndef CLIENTCLASS_H_INCLUDED
+#define CLIENTCLASS_H_INCLUDED
+
+#include "basicAccount.h"
+#include "savingsAccountClass.h"
+#include <iostream>
+using namespace std;
+
+class client
+{
+private:
+    bool is_saving;
+    string name;
+    string address;
+    string phone;
+    savingsAccount savingAccount;
+    bankAccount basicAccount;
+    bankAccount* pBasicAccount = &basicAccount;
+    savingsAccount* pSavingAccount= &savingAccount;
+
+
+public:
+
+    client();
+    client(string name, string address, string phone);
+
+    void set_name(string name);
+    void set_address(string address);
+    void set_phone(string phone);
+    void set_balance(float balance);
+    void set_accountID();
+
+    string get_name();
+    string get_address();
+    string get_phone();
+    int get_accountID();
+    bool get_saving_flag();
+
+    void deposit(float amount);
+    void withdraw(float amount);
+
+    void displayAccount();
+    void display();
+
+    void is_savings_flag();
+};
+
+
+#endif // CLIENTCLASS_H_INCLUDED
